8488c870c3b208e20d9ff61ac2b9e4aaa2126760
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2016-07-07 00:15:31 +0800

    change phpold function name

--- wizhi-multi-filter.php
+++ wizhi-multi-filter.php
@@ -14,12 +14,12 @@ define( 'WIZHI_FILTER', plugin_dir_path( __FILE__ ) );
 
 
 if(version_compare(phpversion(), '5.6.0', '<')) {
-	function bcn_phpold(){
+	function wizhi_filters_phpold(){
 		printf('<div class="error"><p>' . __('您当前的PHP版本（%1$s）不符合插件要求, 请升级到 PHP %2$s 或更新的版本， 否则插件没有任何作用。', 'wizhi') . '</p></div>', phpversion(), '5.6.0');
 	}
 	// 显示警告信息
 	if(is_admin()){
-		add_action('admin_notices', 'bcn_phpold');
+		add_action('admin_notices', 'wizhi_filters_phpold');
 	}
 	return;
 }
