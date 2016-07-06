262200d73e23ce240f22f501bbdf7042aa557383
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2016-07-07 00:08:21 +0800

    add php version require

--- wizhi-multi-filter.php
+++ wizhi-multi-filter.php
@@ -12,6 +12,18 @@ License URI:        http://opensource.org/licenses/MIT
 
 define( 'WIZHI_FILTER', plugin_dir_path( __FILE__ ) );
 
+
+if(version_compare(phpversion(), '5.6.0', '<')) {
+	function bcn_phpold(){
+		printf('<div class="error"><p>' . __('您当前的PHP版本（%1$s）不符合插件要求, 请升级到 PHP %2$s 或更新的版本， 否则插件没有任何作用。', 'wizhi') . '</p></div>', phpversion(), '5.6.0');
+	}
+	// 显示警告信息
+	if(is_admin()){
+		add_action('admin_notices', 'bcn_phpold');
+	}
+	return;
+}
+
 // 添加更新通知
 add_action('in_plugin_update_message-wizhi-multi-filters/wizhi-multi-filter.php', 'wizhi_filter_show_update_notice', 10, 2);
 function wizhi_filter_show_update_notice($current_plugin_data, $new_plugin_data){
